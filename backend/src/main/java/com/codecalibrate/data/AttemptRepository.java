095e9bb70f370036c2376197e3369b5821b30dd8
garretgonzales <garret915@gmail.com>
2026-08-31 12:40:56 -0500

    added attempt check for exercise recommendations- test passing

--- backend/src/main/java/com/codecalibrate/data/AttemptRepository.java
+++ backend/src/main/java/com/codecalibrate/data/AttemptRepository.java
@@ -1,6 +1,11 @@
 package com.codecalibrate.data;
 
 import com.codecalibrate.models.Attempt;
+import com.codecalibrate.models.Exercise;
+import com.codecalibrate.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AttemptRepository extends JpaRepository<Attempt, Integer> {}
+public interface AttemptRepository extends JpaRepository<Attempt, Integer> {
+
+    boolean existsByUserAndExercise(User user, Exercise exercise);
+}
